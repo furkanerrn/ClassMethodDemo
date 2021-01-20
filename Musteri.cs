1a04f172cf292e5c6e37d9acd0d40493719595db
furkanerrn <63072856+furkanerrn@users.noreply.github.com>
2021-01-20 20:33:30 +0300

    Add files via upload

---
+++ Musteri.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo1
+{
+    public class Musteri
+    {
+        public Guid musteriıd { get; set; }
+        public string ad { get; set; }
+        public string soyad { get; set; }
+
+        public int yas { get; set; }
+
+        public string Job { get; set; }
+        public DateTime kayıttarih { get; set; }
+        public bool aktifmi { get; set; }
+    }
+}
